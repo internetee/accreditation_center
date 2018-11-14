be04d0e79776383fa202a4e8e12ceeede2a6d046
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-14 16:44:19 +0200

    Add questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_14_133512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "text"
+  end
 
 end
