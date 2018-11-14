2f8878be2526903001a1c6f275153d2c213a55ee
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-14 17:33:44 +0200

    Add answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_133512) do
+ActiveRecord::Schema.define(version: 2018_11_14_150747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.string "text", null: false
+    t.boolean "correct", default: false, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
   end
 
+  add_foreign_key "answers", "questions"
 end
