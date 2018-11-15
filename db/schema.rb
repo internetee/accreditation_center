7024681dc871067ddb425229baeb2d623c63c964
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-15 20:22:54 +0200

    Rename column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_153909) do
+ActiveRecord::Schema.define(version: 2018_11_15_181842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_153909) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "text"
+    t.string "text_en"
     t.text "comment"
   end
 
