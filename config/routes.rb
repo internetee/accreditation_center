cfa1fbd9f43fd7a369182e4a53e844799e7caff8
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-15 19:40:48 +0200

    Add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     resources :questions
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
 end
