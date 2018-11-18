aaf2d8f9f7fe169d98c3e121b2cb5163f2e6ddde
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-19 01:39:15 +0200

    Load all translations from subfolders

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module AccreditationCenter
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
