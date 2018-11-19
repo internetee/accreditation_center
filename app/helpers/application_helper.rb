3b3c44100cf88ddd2dcdf92ee5e33d114a4f9f6f
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-19 22:49:36 +0200

    Generate <body> CSS class for every action for better styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def body_css_class
+    controller_part = controller_path.split('/').map!(&:dasherize)
+    action_part = action_name.dasherize
+    [controller_part, action_part, 'page'].join('-')
+  end
 end
