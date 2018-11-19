9bb61ed26721f2b4dd41b40f99e17dd4c8b4c25a
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-19 22:14:51 +0200

    Restore `driven_by` setting

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,5 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :rack_test
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
