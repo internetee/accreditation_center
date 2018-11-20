ec672a3b36ecd8a33ada972dd5f4610cf9b7761c
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-20 14:17:31 +0200

    Create `users` database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_193208) do
+ActiveRecord::Schema.define(version: 2018_11_20_121510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 2018_11_19_193208) do
     t.datetime "start", null: false
     t.datetime "end", null: false
     t.boolean "passed", default: false, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
   add_foreign_key "answered_questions", "answers"
