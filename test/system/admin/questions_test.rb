d187f87ea0c4bbb12c92a1f64f24861a3fb27f18
Artur Beljajev <artur.beljajev@internet.ee>
2018-11-21 18:37:44 +0200

    Reformat

--- test/system/admin/questions_test.rb
+++ test/system/admin/questions_test.rb
@@ -6,33 +6,33 @@ class Admin::QuestionsTest < ApplicationSystemTestCase
     @admin_question = questions(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit admin_questions_url
-    assert_selector "h1", text: "Questions"
+    assert_selector 'h1', text: 'Questions'
   end
 
-  test "creating a Question" do
+  test 'creating a Question' do
     visit admin_questions_url
-    click_on "New Question"
+    click_on 'New Question'
 
-    fill_in "question[text_en]", with: 'test'
-    fill_in "question[text_et]", with: 'test'
+    fill_in 'question[text_en]', with: 'test'
+    fill_in 'question[text_et]', with: 'test'
 
-    fill_in "question[answers_attributes][0][text_en]", with: 'test'
-    fill_in "question[answers_attributes][0][text_et]", with: 'test'
+    fill_in 'question[answers_attributes][0][text_en]', with: 'test'
+    fill_in 'question[answers_attributes][0][text_et]', with: 'test'
 
-    click_on "Create Question"
+    click_on 'Create Question'
 
-    assert_text "Question was successfully created"
+    assert_text 'Question was successfully created'
   end
 
-  test "updating a Question" do
+  test 'updating a Question' do
     visit admin_questions_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "Text en", with: 'new text en'
-    click_on "Update Question"
+    fill_in 'Text en', with: 'new text en'
+    click_on 'Update Question'
 
-    assert_text "Question was successfully updated"
+    assert_text 'Question was successfully updated'
   end
 end
