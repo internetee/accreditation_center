71798f1638eb47421b4ca476be33a2d1319cfda7
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-07-16 12:11:47 +0300

    added view for rendering categories, quizzes and questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :quiz, only: [ :show ]
+  resources :categories, only: [ :show ]
+
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_131940) do
+ActiveRecord::Schema.define(version: 2021_07_16_071248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_131940) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "quiz_id"
+    t.boolean "multiply"
     t.index ["quiz_id"], name: "index_categories_on_quiz_id"
   end
 
