bff792576ec96e124c652507cfb840c3e6654e2b
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-08-18 10:45:19 +0300

    fix tests

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,3 @@
-// const { environment } = require('@rails/webpacker')
-
-// module.exports = environment
-
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack');
--- spec/requests/practice_spec.rb
+++ spec/requests/practice_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
+require_relative "../support/devise"
 
 RSpec.describe "Practices", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/practice/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /contact" do
-    it "returns http success" do
-      get "/practice/contact"
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
