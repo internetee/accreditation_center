76241f71b65fc239f89c11624c6806bf5dee3d4b
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-08-18 15:38:14 +0300

    added test for quiz controller

--- spec/controllers/interactions/generate_question_spec.rb
+++ spec/controllers/interactions/generate_question_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe GenerateQuestions do
 	login_user
 
 	let(:category) { build(:category) }
-	let(:question) { build(:question, category: category) }
+	let(:question) { build(:question) }
 	let(:quiz) { build(:quiz) }
+	let(:template_setting_display) { build(:template_setting_display) }
 
 	it "should generate questions for user" do
 		expect(@user.user_questions.count).to eq(0)
@@ -18,5 +19,24 @@ RSpec.describe GenerateQuestions do
 		GenerateQuestions.process(user: @user, quiz: quiz)
 
 		expect(@user.user_questions.count).to eq(20)
+	end
+
+	it "should generate questions for user by template" do
+		quiz.save
+		category.quiz = quiz
+		category.save
+		
+		question.category = category
+		question.save
+		category.questions << question
+		
+		template_setting_display.category = category
+		template_setting_display.display = true
+		template_setting_display.count = 1
+		template_setting_display.save
+
+		GenerateQuestions.process(user: @user, quiz: quiz)
+
+		expect(@user.user_questions.count).to eq(1)
 	end
 end
