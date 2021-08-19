16f709499b3118f1cfafb3d8f3e7a39698299c93
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-08-19 14:15:49 +0300

    added login tests

--- spec/controllers/users/session_controller_spec.rb
+++ spec/controllers/users/session_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require_relative "../../support/devise"
 
 RSpec.describe Users::SessionsController, type: :controller do
 
@@ -18,4 +19,50 @@ RSpec.describe Users::SessionsController, type: :controller do
 			}.to_not change { User.all.count }
 		end
   end
+
+	context "checking protected methods" do
+		login_user
+
+		it 'checking existing username' do
+			result = {
+				"code" => 1000,
+				"data" => {
+					"username" => "#{@user.username}"
+				}
+			}
+
+			@controller = Users::SessionsController.new
+
+			allow(@controller).to receive(:sign_in).with(@user).and_return("Success")
+
+			result = @controller.send(:checking_username, result)
+			expect(result).to eq("Success")
+		end
+
+		it 'should return new user and new quiz for him' do
+			username = "Alyosha"
+			result = {
+				"code" => 1000,
+				"data" => {
+					"username" => "#{username}"
+				}
+			}
+
+			@controller = Users::SessionsController.new
+
+			expect(User.all.count).to eq(1)
+
+			allow(@controller).to receive(:sign_in).and_return("Success")
+			@controller.send(:checking_username, result)
+
+			expect(User.all.count).to eq(2)
+
+			alyosha = User.find_by(username: username)
+			expect(alyosha.quizzes.count).to eq(2)
+
+			# When created user should be created two quizzes - theory quiz and practical quiz
+			expect(alyosha.quizzes.where(theory: true).count).to eq(1)
+			expect(alyosha.quizzes.where(theory: false).count).to eq(1)
+		end
+	end
 end
