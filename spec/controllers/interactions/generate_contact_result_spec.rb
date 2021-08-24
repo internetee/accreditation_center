aafdeabf80189c50b5c24b01f01eeba80ed34f78
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-08-24 11:31:00 +0300

    refactored tests

--- spec/controllers/interactions/generate_contact_result_spec.rb
+++ spec/controllers/interactions/generate_contact_result_spec.rb
@@ -4,35 +4,171 @@ require_relative "../../support/devise"
 RSpec.describe GenerateContactResult do
 	login_user
 
-	it "should return result true" do
-		response = {
-			"code" => 1000
-		}
+	context "valid data" do
+		it "checking priv contact should return true" do
+			response = {
+				"code" => 1000,
+				"data" => {
+					"name" => "PRIV",
+					"email" => "PRIV@eesti.ee",
+					"phone" => "+372.51234567",
+					"ident" => {
+						"type" => "priv",
+						"code" => "49708260212"
+					}
+				}
+			}
 
-		token = "example-2233"
-		api_connector = GetContact.new(token)
+			token = "example-2233"
+			api_connector = GetContact.new(token)
 
-		allow(api_connector).to receive(:request).and_return(response)
-		allow(api_connector).to receive(:contact_endpoint).and_return("http://example.api")
+			allow(api_connector).to receive(:request).and_return(response)
+			allow(api_connector).to receive(:contact_endpoint).and_return("http://example.api")
 
-		result = GenerateContactResult.process(contact_id: "AABB", api_connector: api_connector, action: "contact", user: @user)
-		
-		expect(result).to be true
+			allow(GenerateContactResult).to receive(:check_org_contact).and_return(true)
+
+			hash = {
+				org_contact_id: "AABB",
+				priv_contact_id: "BBAA",
+				api_connector: api_connector,
+				action: "contact",
+				user: @user,
+				priv_name: "PRIV",
+				org_name: "ORG"
+			}
+
+			result = GenerateContactResult.checking_data(hash)
+			
+			expect(result).to be true
+		end
+
+		it "checking org contact should return true" do
+			response = {
+				"code" => 1000,
+				"data" => {
+					"name" => "ORG",
+					"email" => "PRIV@eesti.ee",
+					"phone" => "+372.51234567",
+					"ident" => {
+						"type" => "org",
+						"code" => "49708260212"
+					}
+				}
+			}
+
+			token = "example-2233"
+			api_connector = GetContact.new(token)
+
+			allow(api_connector).to receive(:request).and_return(response)
+			allow(api_connector).to receive(:contact_endpoint).and_return("http://example.api")
+
+			allow(GenerateContactResult).to receive(:check_private_contact).and_return(true)
+
+			hash = {
+				org_contact_id: "AABB",
+				priv_contact_id: "BBAA",
+				api_connector: api_connector,
+				action: "contact",
+				user: @user,
+				priv_name: "PRIV",
+				org_name: "ORG"
+			}
+
+			result = GenerateContactResult.checking_data(hash)
+			
+			expect(result).to be false
+		end
 	end
 
-	it "should return result false" do
-		response = {
-			"code" => 2000
-		}
+	context "invalid data" do
+		before(:each) do
+			token = "example-2233"
+			@api_connector = GetContact.new(token)
 
-		token = "example-2233"
-		api_connector = GetContact.new(token)
+			@hash = {
+				org_contact_id: "AABB",
+				priv_contact_id: "BBAA",
+				api_connector: @api_connector,
+				action: "contact",
+				user: @user,
+				priv_name: "PRIV",
+				org_name: "ORG"
+			}
 
-		allow(api_connector).to receive(:request).and_return(response)
-		allow(api_connector).to receive(:contact_endpoint).and_return("http://example.api")
 
-		result = GenerateContactResult.process(contact_id: "AABB", api_connector: api_connector, action: "contact", user: @user)
-		
-		expect(result).to be false
+		end
+
+		it "response with diffent data for priv should return false" do
+			@response = {
+				"code" => 1000,
+				"data" => {
+					"name" => "FAIL",
+					"email" => "FAIL@eesti.ee",
+					"phone" => "+372.666",
+					"ident" => {
+						"type" => "priv",
+						"code" => "000000"
+					}
+				}
+			}
+
+			allow(@api_connector).to receive(:request).and_return(@response)
+			allow(@api_connector).to receive(:contact_endpoint).and_return("http://example.api")
+
+			allow(GenerateContactResult).to receive(:check_org_contact).and_return(true)
+
+			result = GenerateContactResult.checking_data(@hash)
+			
+			expect(result).to be false
+		end
+
+		it "response with diffent data for org should return false" do
+			@response = {
+				"code" => 1000,
+				"data" => {
+					"name" => "FAIL",
+					"email" => "FAIL@eesti.ee",
+					"phone" => "+372.666",
+					"ident" => {
+						"type" => "org",
+						"code" => "000000"
+					}
+				}
+			}
+
+			allow(@api_connector).to receive(:request).and_return(@response)
+			allow(@api_connector).to receive(:contact_endpoint).and_return("http://example.api")
+
+			allow(GenerateContactResult).to receive(:check_private_contact).and_return(true)
+
+			result = GenerateContactResult.checking_data(@hash)
+			
+			expect(result).to be false
+		end
+
+		it "response with diffent code should return false" do
+			@response = {
+				"code" => 1001,
+				"data" => {
+					"name" => "BBAA",
+					"email" => "BBAA@eesti.ee",
+					"phone" => "+372.51234567",
+					"ident" => {
+						"type" => "priv",
+						"code" => "49708260212"
+					}
+				}
+			}
+
+			allow(@api_connector).to receive(:request).and_return(@response)
+			allow(@api_connector).to receive(:contact_endpoint).and_return("http://example.api")
+
+			allow(GenerateContactResult).to receive(:check_org_contact).and_return(true)
+
+			result = GenerateContactResult.checking_data(@hash)
+			
+			expect(result).to be false
+		end
+
 	end
 end
---
+++ spec/controllers/practice/contact_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require_relative "../../support/devise"
+
+RSpec.describe Practice::ContactController, type: :controller do
+	let(:result) { build(:result) }
+	let(:category) { build(:category) }
+	let(:another_user) { build(:user) }
+	let(:user_answer) { build(:user_answer) }
+
+	let(:answer_question) { build(:answer_question) }
+	let(:question) { build(:question) }
+	let(:answer) { build(:answer) }
+
+	let(:practice) { build(:practice, user: @user) }
+
+	context 'render pages' do
+	login_user
+
+		it 'should be successfully contact practice rendered if practice did not found' do
+			get :index
+			expect(response).to have_http_status(:ok)
+		end
+
+		it 'should be successfully contact practice rendered if practice found and result true' do
+			practice.action_name = "contact"
+			practice.result = true
+
+			practice.save
+			
+			get :index
+			expect(response).to have_http_status(:ok)
+		end
+
+		it 'should be successfully contact practice rendered if practice found and result false' do
+			practice.action_name = "contact"
+			practice.result = false
+
+			practice.save
+			
+			get :index
+			expect(response).to have_http_status(:ok)
+		end
+	end
+end
