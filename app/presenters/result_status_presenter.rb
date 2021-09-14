f824ed9df6c7ff992ea40fc4c4d099ec8c39f7c4
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2021-09-14 12:12:42 +0300

    added new forms and cards

--- app/presenters/result_status_presenter.rb
+++ app/presenters/result_status_presenter.rb
@@ -12,21 +12,21 @@ class ResultStatusPresenter
     if @quiz.theory
       return generate_tag(@quiz.result.result) if @quiz.result
 
-      content_tag(:span, "Waiting for chellenge")
+      content_tag(:span, "Waiting for chellenge", class: "text-sm px-3 py-1 bg-indigo-200 text-indigo-800 rounded-full")
 
     else
       @in_proccess = Practice.find_by(user: user, action_name: 'domain')
       @practice = PracticeResult.find_by(user: user)
       return generate_tag(@practice.result) unless @practice.nil?
 
-      return content_tag(:span, "In Proccess", class: "answered") unless @in_proccess.nil? 
-      content_tag(:span, "Waiting for chellenge")
+      return content_tag(:span, "In Proccess", class: "text-sm px-3 py-1 bg-red-200 text-red-800 rounded-full") unless @in_proccess.nil? 
+      content_tag(:span, "Waiting for chellenge", class: "text-sm px-3 py-1 bg-indigo-200 text-indigo-800 rounded-full")
     end
   end
 
 
   def generate_tag(flag)
-    return content_tag(:span, "True", class: "green") if flag
-	  content_tag(:span, "Fails", class: "answered")
+    return content_tag(:span, "Passed", class: "text-sm px-3 py-1 bg-green-200 text-green-800 rounded-full") if flag
+	  content_tag(:span, "Failed", class: "text-sm px-3 py-1 bg-gray-200 text-gray-800 rounded-full")
   end
 end
