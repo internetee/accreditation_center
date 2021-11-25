82958f47c19e5a075a19f30595b1f25995c1f9f8
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2021-11-25 10:36:23 +0200

    remove production registry base url

--- app/services/results.rb
+++ app/services/results.rb
@@ -6,8 +6,7 @@ class Results < ApiConnector
 	end
 
 	def push_endpoint
-		base_url = ENV['BASE_PRODUCTION_URL']
-		base_url = ENV['BASE_URL'] if Rails.env.staging?
+		base_url = ENV['BASE_URL']
 		endpoint = ENV['PUSH_RESULT'] 
 
 		base_url + endpoint
