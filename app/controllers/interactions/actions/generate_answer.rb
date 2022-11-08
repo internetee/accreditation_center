2267c67c32e7d6abb631627ed793cb8aeff030bb
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-11-08 14:07:31 +0200

    fixed conflicts

--- app/controllers/interactions/actions/generate_answer.rb
+++ app/controllers/interactions/actions/generate_answer.rb
@@ -1,18 +1,17 @@
 module GenerateAnswer
-	extend self
+  extend self
 
-	def process(answer_question_params:, user_answer:, quiz_id:)
-		quiz = Quiz.find(quiz_id)
-		if answer_question_params[:answer_id].kind_of?(Array)
-			MultipleAnswerCreate.create_multiple_answer(answer_question_params: answer_question_params,
-																															user_answer: user_answer)
-		else
-			SingleAnswerCreate.create_single_answer(answer_question_params: answer_question_params,
-																															user_answer: user_answer)
-		end
+  def process(answer_question_params:, user_answer:, quiz_id:)
+    quiz = Quiz.find(quiz_id)
+    if answer_question_params[:answer_id].is_a?(Array)
+      MultipleAnswerCreate.create_multiple_answer(answer_question_params: answer_question_params,
+                                                  user_answer: user_answer)
+    else
+      SingleAnswerCreate.create_single_answer(answer_question_params: answer_question_params,
+                                              user_answer: user_answer)
+    end
 
-		p "========="
-		p QuestionId.generate_id_for_new_question(question_id: answer_question_params[:question_id],
-																											user_answer: user_answer, quiz: quiz)
-	end
+    p QuestionId.generate_id_for_new_question(question_id: answer_question_params[:question_id],
+                                              user_answer: user_answer, quiz: quiz)
+  end
 end
