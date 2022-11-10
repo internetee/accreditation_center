c798869a57fd02e1877219a92b576fb37ac6479b
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-11-10 14:09:23 +0200

    fixed key hash errors

--- app/services/practice_services/generate_transfer_code_service.rb
+++ app/services/practice_services/generate_transfer_code_service.rb
@@ -6,10 +6,11 @@ module PracticeServices
     def call
       domain_creator = CreateDomain.new(username: USERNAME, password: PASSWORD)
       response = domain_creator.create_domain
+
       if response['code'] == 2202 || response['code'] == 2104 || response['code'] == 2005
         response(success: false, errors: response['message'])
       else
-        response(success: true, paylaod: response.dig('data', 'domain'))
+        response(success: true, payload: response.dig('data', 'domain'))
       end
     end
 
